c2887f6e6e66ee372848d0c8d4524453c2623bba
KUtley-TP <kyle.utley@trueprocess.com>
2013-12-06 09:57:38 -0600

    Added new Static class and method for printing to console

--- GitTesting/TestGitConsoleProject/Program.cs
+++ GitTesting/TestGitConsoleProject/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TestGitConsoleProject.Utils;
 
 namespace TestGitConsoleProject
 {
@@ -9,7 +10,7 @@ namespace TestGitConsoleProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("First Change didn't work, trying a third time, using git bash to commit");
+            LinePrinter.PrintLine("First Change didn't work, trying a third time, using git bash to commit");
         }
     }
 }
---
+++ GitTesting/TestGitConsoleProject/Utils/LinePrinter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGitConsoleProject.Utils
+{
+    public static class LinePrinter
+    {
+        public static void PrintLine(string stuffToPrint)
+        {
+            Console.WriteLine(stuffToPrint);
+        }
+    }
+}
